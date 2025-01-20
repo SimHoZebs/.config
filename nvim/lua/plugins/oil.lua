ce9825cfef7371dabe2081808411557f86ed478c
Ho Sim <hsim.sram.com>
2025-01-20 17:14:34 +0000

    oil; show hidden files

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,5 +1,5 @@
 return {
-  'stevearc/oil.nvim',
+  "stevearc/oil.nvim",
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {},
@@ -8,7 +8,16 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   config = function()
     require("oil").setup({
-      vim.keymap.set('n', '<leader>so',"<CMD>Oil<CR>", { desc = '[S]earch [O]il' }),
+      vim.keymap.set(
+        "n",
+        "<leader>so",
+        "<CMD>Oil<CR>",
+        { desc = "[S]earch [O]il" }
+      ),
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      },
     })
   end,
 }
