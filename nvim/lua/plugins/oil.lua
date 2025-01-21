912b9932a2f9eec9efe6813d7f0d637e7495b08d
Ho Sim <hsim.sram.com>
2025-01-21 15:19:02 +0000

    default stylua formatting

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,23 +1,18 @@
 return {
-  "stevearc/oil.nvim",
+  'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {},
   -- Optional dependencies
-  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   config = function()
-    require("oil").setup({
-      vim.keymap.set(
-        "n",
-        "<leader>so",
-        "<CMD>Oil<CR>",
-        { desc = "[S]earch [O]il" }
-      ),
+    require('oil').setup {
+      vim.keymap.set('n', '<leader>so', '<CMD>Oil<CR>', { desc = '[S]earch [O]il' }),
       view_options = {
         -- Show files and directories that start with "."
         show_hidden = true,
       },
-    })
+    }
   end,
 }
